765f62ce149f64567b662bbdb36d18cb6bcadffa
Derek Peruo <derek@derekperuo.net>
2017-08-17 18:06:58 +0000

    Update python.py

--- python.py
+++ python.py
@@ -5,8 +5,8 @@
 array = [1, 2, 3, 0, 4, 5]
 
 def main():
-  for i in array:
-    print 'hello #{0}'.format(i)
+    for i in array:
+        print 'hello #{0}'.format(i)
 
 if __name__ == '__main__':
-  main()
+    main()
